00ff1d7584502b3f46dcff05711e6073a7ea97c9
Felipe Andrade <carlos.andrade03@epn.edu.ec>
2020-07-23 15:58:53 -0500

    creacion de usuario y standart restful

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HttpJuegoModule} from "./http/http-juego.module";
+import {UsuarioModule} from "./usuario/usuario.module";
 
 @Module({
   imports: [
-      HttpJuegoModule
+      HttpJuegoModule,
+      UsuarioModule
     // aqui van otros modulos importados.
   ],
   controllers: [
