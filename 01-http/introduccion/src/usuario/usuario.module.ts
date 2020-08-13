763532b4e70ad1dde116d1a864bd9ab03b14ea7c
Felipe Andrade <carlos.andrade03@epn.edu.ec>
2020-08-13 01:22:56 -0500

    configurar service y module para mysql

--- 01-http/introduccion/src/usuario/usuario.module.ts
+++ 01-http/introduccion/src/usuario/usuario.module.ts
@@ -1,11 +1,19 @@
 import {Module} from "@nestjs/common";
 import {UsuarioController} from "./usuario.controller";
 import {UsuarioService} from "./usuario.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario.entity";
 
 
 
 @Module({
     imports: [
+        TypeOrmModule.forFeature(
+            [
+                UsuarioEntity
+            ],
+            'default' //nombre de la cadena de conexion
+        )
 
     ],
     controllers: [
